409f9607bcadcf7b17e62868f0867864d2fe6429
Jani Mikkonen <jani.mikkonen@siili.com>
2019-01-16 20:22:33 +0200

    Que all messages to status endpoint

--- src/GuiStatus/GuiStatusListener.py
+++ src/GuiStatus/GuiStatusListener.py
@@ -6,9 +6,19 @@ class GuiStatusListener(object):
     ROBOT_LISTENER_API_VERSION = 2
 
     def _POST(self, payload):
-        res = post(self.endpoint, json=json.dumps(payload))
+        self.queue.append(payload)
+        current = None
+        try:
+            while self.queue:
+                current = self.queue.pop()
+                res = post(self.endpoint, json=json.dumps(current))
+        except:
+            if current is not None:
+                self.queue.append(current)
+
 
     def __init__(self, endpoint="http://127.0.0.1:31337"):
+        self.queue = []
         self.endpoint = endpoint
 
     def start_suite(self, name, attrs):
